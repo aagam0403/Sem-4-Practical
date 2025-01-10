0b98c15d97f48772d49889155b1447089d4f54da
Aagam Sanghvi <aagamsanghvi0403@gmail.com>
2025-01-10 10:15:00 +0530

    Add files via upload

---
+++ Java Language Topic Wise Demo/Unit 2/LinkList.java
@@ -0,0 +1,50 @@
+import java.util.*;
+class LinkList
+{
+    public static void main(String[] args) 
+    {
+        LinkedList<Integer> l = new LinkedList<>();
+        
+        //Creating linklist and adding values to linked list
+        l.add(25);
+        l.add(78);
+        l.add(24);
+        System.out.println(l);
+
+        //Adding value to first node
+        l.addFirst(1);
+        System.out.println(l);
+
+        //Adding value to last node
+        l.addLast(4);
+        System.out.println(l);
+
+        //clear contains
+        System.out.println(l.contains(1));
+
+        //Getting value from link list
+        System.out.println(l.get(2));
+        System.out.println(l.getFirst());
+        System.out.println(l.getLast());
+
+        //Removing values from link list
+        System.out.println(l.remove(2));
+        System.out.println(l.removeFirst());
+        System.out.println(l.removeLast());
+        System.out.println(l);
+
+        //Size of Linked List
+        System.out.println(l.size());
+
+        //Sort the values of linked list
+        l.add(46);
+        l.add(2);
+        l.add(32);
+        Collections.sort(l);
+        System.out.println(l);
+
+        //Clear Linked List
+        l.clear();
+        System.out.println(l);
+    }
+}
