662c55fef3efbe80baa0da725a73a1dadfca0208
Aagam Sanghvi <aagamsanghvi0403@gmail.com>
2025-01-21 10:58:25 +0530

    Add files via upload

---
+++ Java Language Topic Wise Demo/Unit 2/ForEach.java
@@ -0,0 +1,19 @@
+public class ForEach 
+{
+    void setData(String... n)
+    {
+        System.out.println();
+        System.out.println("Subject Name = ");
+        for(String  i : n) //n value shifts to variable i (one by one)
+        {
+            System.out.println(i);
+        }
+    }
+    public static void main(String args[])
+    {
+        ForEach e = new ForEach();
+        e.setData("PHP");
+        e.setData("PHP","Core Java");
+        e.setData("PHP","CoreJava","Python");
+    }  
+}
