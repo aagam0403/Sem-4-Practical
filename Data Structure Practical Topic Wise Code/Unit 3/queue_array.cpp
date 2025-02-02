e20d1fdb6b4ba1e8c7ce01c2bf490af3eebeabc6
Aagam Sanghvi <aagamsanghvi0403@gmail.com>
2025-02-02 22:11:28 +0530

    Update queue_array.cpp

--- Data Structure Practical Topic Wise Code/Unit 3/queue_array.cpp
+++ Data Structure Practical Topic Wise Code/Unit 3/queue_array.cpp
@@ -1,87 +1,93 @@
-#include <iostream>
-#define MAX 101
+#include<iostream>
+#define MAX 10
+// #include <queue>
 using namespace std;
 int queue[MAX];
 int front = -1, rear = -1;
-void enqueue() {
-    int num;
-    cout << "Enter number to insert: ";
-    cin >> num;
-
-    if (rear == MAX - 1) {
-        cout << "Queue is FULL (Overflow)\n";
-        return;
-    }
-
-    if (front == -1) front = 0;
-    rear++;
-    queue[rear] = num;
-    cout << num << " added to queue.\n";
-}
-
-void dequeue() 
+void enqueue()
 {
-    if (front == -1 || front > rear) 
-    {
-        cout << "Queue is EMPTY (Underflow)\n";
-        return;
-    }
-    cout << "Removed: " << queue[front] << endl;
-    if (front == rear)
-    {
-        front = rear = -1;
-    }
-    else
-    {
-        front++;
-    }
+	int num;
+	cout<<"Enter the Number to be inserted in the queue";
+	cin>>num;
+	if(rear == MAX-1)
+	{
+		cout<<"Queue is overflow";
+	}
+	else
+	{
+		if(front == -1 && rear == -1)
+		{
+			front = rear = 0;
+		}
+		else
+		{
+			rear++;
+		}
+		queue[rear]=num;
+	}
 }
-
-void peek() 
+int dequeue()
 {
-    if (front == -1 || front > rear)
-    {
-        cout << "Queue is EMPTY\n";
-    }
-    else
-    {
-        cout << "Front element: " << queue[front] << endl;
-    }
+	int val;
+	if(front == -1)
+	{
+		cout<<endl<<"Queue Underflow Empty";
+	}
+	else
+	{
+		val = queue[front];
+		front++;
+	}
+	return val;
 }
-
-void display() 
+int peek()
 {
-    if (front == -1 || front > rear) 
-    {
-        cout << "Queue is EMPTY\n";
-        return;
-    }
-    cout << "Queue elements: ";
-    for (int i = front; i <= rear; i++)
-    {
-        cout << queue[i] << " ";
-    }
-    cout << endl;
+	return (queue[front]);
 }
-
-int main() 
+void display()
 {
-    int choice;
-    do 
-    {
-        cout << "\nMENU:\n1. Insert\n2. Delete\n3. Peek\n4. Display\n5. Exit\nEnter choice: ";
-        cin >> choice;
+	if(front == -1 || front>rear)
+	{
+		cout<<"Queue is Empty Undeflow";
+	}
+	else
+	{
+		cout<<endl;
+		for(int i=front; i<=rear; i++)
+		{
+			cout<<queue[i]<<" ";
+		}
+	}
+}
+int main()
+{
+	int val,option;
+	do
+	{
+		cout<<endl<<"***MAIN MENU***"<<endl;
+		cout<<"1: Insert a Element"<<endl;
+		cout<<"2: Delete a Element"<<endl;
+		cout<<"3: Peek"<<endl;
+		cout<<"4: Display"<<endl;
+		cout<<"5: Exit"<<endl;
+		cin>>option;
+		switch(option)
+		{
+		case 1: enqueue();
+			break;
 
-        switch (choice) 
-        {
-            case 1: enqueue(); break;
-            case 2: dequeue(); break;
-            case 3: peek(); break;
-            case 4: display(); break;
-            case 5: cout << "Exiting...\n"; break;
-            default: cout << "Invalid choice! Try again.\n";
-        }
-    } 
-    while (choice != 5);
-    return 0;
+		case 2: val=dequeue();
+			cout<<"The Value deleted from the queue is: "<<val<<endl;
+			break;
+
+		case 3: val = peek();
+			cout<<"The First Value is the queue is: "<<val;
+			break;
+
+		case 4: display();
+			break; 
+		}
+	}
+	while(option != 5);
+	return 0;
 }
