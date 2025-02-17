6b76ae38050002c8c0a21fc3cca09c7cf3b103ed
Aagam Sanghvi <aagamsanghvi0403@gmail.com>
2025-02-17 22:39:44 +0530

    Add files via upload

---
+++ Java Language Topic Wise Demo/Unit 4/TryCatchException.java
@@ -0,0 +1,30 @@
+//Try Catch (Exception handling)
+//Divide by zero error
+import java.util.*;
+class DivideClass
+{
+    float Divide(int a, int b)
+    {
+        return a/b;
+    }
+}
+class TryCatchException
+{
+    public static void main(String[] args) 
+    {
+        DivideClass d = new DivideClass();
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter A = ");
+        int a = s.nextInt();
+        System.out.print("Enter B = ");
+        int b = s.nextInt();
+        try
+        {
+            System.out.println(d.Divide(a,b));
+        }
+        catch(Exception e)
+        {
+            System.out.println("Divide by 0 error");
+        }
+    }
+}
