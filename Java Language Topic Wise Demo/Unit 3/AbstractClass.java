3f6fbb0ca80b870c075620a5fccb0da8515ff74c
Aagam Sanghvi <aagamsanghvi0403@gmail.com>
2025-03-27 09:40:49 +0530

    Update AbstractClass.java

--- Java Language Topic Wise Demo/Unit 3/AbstractClass.java
+++ Java Language Topic Wise Demo/Unit 3/AbstractClass.java
@@ -12,7 +12,6 @@ class DummyClass extends Dummy
     int b=2;
     void display1()
     {
-        //super.display();
         System.out.println("B : "+b);
     }
 }
@@ -20,8 +19,8 @@ class AbstractClass
 {
     public static void main(String[] args) 
     {
-        DummyClass c = new DummyClass();//creating object of parent(abstract) class using child class
+        DummyClass c = new DummyClass();
         c.display();
-        c.display1(); //will give error as parent class cannot access Child class content
+        c.display1(); 
     }
 }
